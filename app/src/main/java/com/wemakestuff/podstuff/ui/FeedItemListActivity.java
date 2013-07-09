345ef7e88ab113560ceb028126a70ab56a56a7d4
David Pate <davidtpate@gmail.com>
2013-07-08 22:09:51 -0400

    Added in default android drawables for lists.

---
+++ app/src/main/java/com/wemakestuff/podstuff/ui/FeedItemListActivity.java
@@ -0,0 +1,8 @@
+package com.wemakestuff.podstuff.ui;
+
+/**
+ * Created with IntelliJ IDEA. User: David Date: 7/8/13 Time: 9:03 PM To change this template use File | Settings | File
+ * Templates.
+ */
+public class FeedItemListActivity {
+}
